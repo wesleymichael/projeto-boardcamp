3d2020e7a5991d95b0d7083a6f2a8f8bf1dfb32e
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-05-11 05:42:35 -0300

    feat: added function to get customer by id

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -8,3 +8,19 @@ export async function getCustomers(req, res){
         res.status(500).send(error.message);
     }
 }
+
+export async function getCustomersById(req, res){
+    const id = parseInt(req.params.id);
+
+    if(isNaN(id)) return res.sendStatus(400);
+    
+    try{
+        const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id]);
+
+        if(customer.rowCount === 0) return res.sendStatus(404);
+
+        res.send(customer.rows[0]);
+    } catch (error){
+        res.status(500).send(error.message);
+    }
+}
