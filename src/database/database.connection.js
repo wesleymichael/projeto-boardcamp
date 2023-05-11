87240d5b0688501eede232f4c6fced29cd3f29bf
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-05-11 02:37:35 -0300

    fix: database connection

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,3 +1,12 @@
-const connection = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+export const db = new Pool(configDatabase);
